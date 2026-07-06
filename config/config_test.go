f6af4cd58e23da102462dd435580b9f9f96e35d7
leerura <sw09112@naver.com>
2026-07-06 14:13:35 +0900

    [FEAT]: mysql 연결

---
+++ config/config_test.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	wantPort := 3333
+	t.Setenv("PORT", fmt.Sprint(wantPort))
+
+	got, err := New()
+
+	if err != nil {
+		t.Fatalf("failed to create config: %v", err)
+	}
+	if got.Port != wantPort {
+		t.Errorf("got %d, want %d", got.Port, wantPort)
+	}
+
+	wantEnv := "dev"
+	if got.Env != wantEnv {
+		t.Errorf("got %q, want %q", got.Env, wantEnv)
+	}
+}
