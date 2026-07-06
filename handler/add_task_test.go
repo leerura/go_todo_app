e95868e23f551c813a879aca656804dce697739b
leerura <sw09112@naver.com>
2026-07-06 14:29:44 +0900

    [FIX] 오타수정

--- handler/add_task_test.go
+++ handler/add_task_test.go
@@ -23,10 +23,10 @@ func TestAddTask(t *testing.T) {
 		want want
 	} {
 		"ok" : {
-			reqFile: "testdata/add_task/ok.req.json.golden",
+			reqFile: "testdata/add_task/ok_req.json.golden",
 			want: want{
 				status: http.StatusOK,
-				rspFile: "testdata/add_task/ok.rsp.json.golden",
+				rspFile: "testdata/add_task/ok_rsp.json.golden",
 			},
 		},
 		"badRequest" : {
